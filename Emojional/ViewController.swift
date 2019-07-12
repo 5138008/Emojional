5f46e086ac1ac4a7f164650aa53964ff8a4cca4d
Shirui Zhong <shirui.us@gmail.com>
2019-07-12 10:53:44 -0700

    Second commit I think

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -9,18 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var customMessages = ["Jazzy" : ["cool song1", "cool song2", "Boop jbhk", "Oh yeah"], "Upbeat" : ["doot doot", "insert good music here"]]
+    var random = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    let emojis = ["🎷" : "Jazz", "🎤" : "Upbeat"]
+    let emojis = ["🎷" : "Jazzy", "🎤" : "Upbeat"]
+//    var random = Int.random(in: 0...1)
+    
     @IBAction func showMessage(sender : UIButton) {
         let selectedEmotion = sender.titleLabel?.text
-        let alertController = UIAlertController(title: "No", message : ["Jazz" : "My Funny Valentine, Chet Baker", "Djangology, Djanjo Reihnhart"], preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "OK", style : UIAlertAction.Style.default, handler: nil))
+        
+        if selectedEmotion == "🎷" {
+            random = Int.random(in : 0...3)
+        }else if selectedEmotion == "🎤" {
+            random = Int.random(in : 0...1)
+        }
+        
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[random]
+        let alertController = UIAlertController(title: "What's up?", message : emojiMessage , preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "Okay", style : UIAlertAction.Style.default, handler: nil))
         present(alertController, animated : true, completion : nil)
 
-}
+    }
+    
 }
 
